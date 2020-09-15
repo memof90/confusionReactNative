1ee50efb246e8314be0ddc6dcc6a2fc5a4a79e42
memo figueredo <memof80@gmail.com>
2020-09-15 16:27:13 -0500

    se agrego contenido al home Compoenent y navegation part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, ScrollView} from 'react-native';
+
+import { Card } from 'react-native-elements'
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+
+function RenderItem(props) {
+    const item = props.item;
+    
+    if (item != null) {
+        return(
+            <Card>
+                <Card.Image source={require('./images/uthappizza.png')}>
+                  <Card.FeaturedTitle>{item.name}</Card.FeaturedTitle>
+                  <Card.FeaturedSubtitle>{item.designation}</Card.FeaturedSubtitle>  
+                </Card.Image>
+                <Text style={{margin: 10}}>{item.description}</Text>
+            </Card>
+        );
+    }
+    else {
+        return(
+            <View></View>
+        );
+    }
+}
 
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            dishes:DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+         }
     }
     render() { 
         return ( 
-            <View><Text>Home Component</Text></View>
+           <ScrollView>
+               <RenderItem item={this.state.dishes.filter((dish)=> dish.featured)[0]}/>
+               <RenderItem item={this.state.promotions.filter((promo)=> promo.featured)[0]}/>
+               <RenderItem item={this.state.leaders.filter((leader)=> leader.featured)[0]}/>
+           </ScrollView>
          );
     }
 }
