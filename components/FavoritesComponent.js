5f9d093ca3bcf094b9116d9ee39d56a033076b30
memo figueredo <memof80@gmail.com>
2020-10-07 15:21:35 -0500

    delete favorites add

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import { View, FlatList, Text} from 'react-native';
-import { Avatar, ListItem , Tile} from 'react-native-elements';
+import { View, FlatList, Text, Animated, StyleSheet} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { Avatar, Icon, ListItem , Tile} from 'react-native-elements';
 import  { Loading }  from './LoadingComponent';
+
+import { deleteFavorite } from '../redux/ActionCreators';
 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+
+
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
 
 
 const mapStateToProps = state => {
@@ -14,15 +21,49 @@ const mapStateToProps = state => {
     }
   }
 
+  const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+})
+
 class Favorites extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+
+
+
+
+    
     render() { 
 
         const renderMenuItem = ({ item, index}) => {
+    
+            const items = item;
+            const renderLefActions = ( progess, dragX)  => {
+                const trans = dragX.interpolate({
+                    inputRange: [0, 50, 100, 101],
+                    outputRange: [-20, 0, 0, 1],
+                });
+                return (
+                    <RectButton style={styles.leftAction} onPress={() => this.props.deleteFavorite(items.id)}>
+                      <Animated.Text
+                        style={[
+                          styles.actionText,
+                          {
+                            transform: [{ translateX: trans }],
+                            color: 'white',
+                            fontWeight: 'bold'
+                          },
+                        ]}>
+                        Delete
+                      </Animated.Text>
+                    </RectButton>
+                  );
+                };
+                
             return(
+                <Swipeable renderLeftActions={renderLefActions}>
                 <ListItem key={index}
                 onPress={() => navigate('Dishdetail', { dishId: item.id })}
                 >
@@ -34,6 +75,8 @@ class Favorites extends Component {
                 <ListItem.Chevron />
 
                 </ListItem>
+                </Swipeable>
+
             );
         }
 
@@ -65,5 +108,14 @@ class Favorites extends Component {
 
     }
 }
+
+const styles = StyleSheet.create({
+
+    leftAction:{
+        backgroundColor:'red',
+        justifyContent:'center',
+        flex:1
+      }
+  });
  
-export default connect(mapStateToProps)(Favorites) ;
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites) ;
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -165,6 +165,11 @@ export const addFavorite = (dishId) => ({
     payload: dishId
 });
 
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});
+
 
 export const addComment = (comment) => ({
     type: ActionTypes.ADD_COMMENT,
